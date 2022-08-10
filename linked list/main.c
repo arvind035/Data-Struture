52110a812d9ff844c4df57e046ab37c85428dfce
arvind035 <98012479+arvind035@users.noreply.github.com>
2022-08-10 11:38:33 +0530

    Add files via upload

---
+++ linked list/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+}*head=NULL;
+
+int main()
+{
+     {
+            struct node *p=head,*r;
+            int x,i=0;
+           printf("enter the index\t");
+           scanf("%d",&x);
+           while(i<x)
+           {
+               r=p;
+               i++;
+               p=p->next;
+           }
+           r->next=p->next;
+        }
+    return 0;
+}
